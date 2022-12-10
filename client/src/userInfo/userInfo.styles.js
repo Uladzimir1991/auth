7583cacff6754d013f378617dfa4c7d423bb3133
Vladimir Nesterovich <80399934+Uladzimir1991@users.noreply.github.com>
2022-12-10 15:22:06 +0300

    Delete client directory

--- client/src/userInfo/userInfo.styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const ButtonLogoutStyled = styled.button`
-  border: none;
-  background: #A0627B;
-  border-radius: 4px;
-  box-shadow: 2px 1px 4px rgba(0,0,0,0.3);
-`
